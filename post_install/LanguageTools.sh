d9b59f9cc3372a2b7913d4dee044514631e851ba
Khalid <Khalid@K.com>
2015-08-27 09:34:27 +0200

    add post_install script

---
+++ post_install/LanguageTools.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+# unzip to 
+
+cible="/home/vagrant/.vim/bundle/vim-grammarous/misc"
+
+#Instal JRE + jdk
+sudo apt-get install default-jre
+
+#Download last version
+
+mkdir $cible
+cd $cible
+wget https://www.languagetool.org/download/LanguageTool-3.0.zip
+unzip *.zip
+rm -f *.zip
+
+#Unzip
