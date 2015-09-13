921ed65e98867963e7c592eae15b383e3e15d4f5
Khalid <Khalid@K.com>
2015-09-13 08:41:37 +0200

    Git basic conf

--- post_install/regular.sh
+++ post_install/regular.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git config --global user.name "Khouli"
+git config --global user.email Khouli@VMdefault.com
+
 git clone https://github.com/Khouli00/vim /home/vagrant/.vim	
 git clone https://github.com/VundleVim/Vundle.vim.git /home/vagrant/.vim/bundle/Vundle.vim
 vim -c VundleInstall -c quitall	
