15720473419d2382c46ca2af57624111c73b350a
Khalid <Khalid@K.com>
2015-11-21 10:12:34 +0100

    added lib post_install/

---
+++ post_install/lib_python.sh
@@ -0,0 +1,12 @@
+
+
+apt-get install libxml2-dev libxslt1-dev python-dev
+pip install lxml
+pip install beautifulsoup4
+
+apt-get install graphviz libgraphviz-dev pkg-config
+pip install pygraphviz
+pip install networkx
+pip install requests
+
+#pip3 scdl
