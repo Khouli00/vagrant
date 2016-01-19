912d56b46d20cc693f45c2f01e3f3d8158b45e96
Khouli00 <supermoik@gmail.Com>
2016-01-19 13:48:52 +0100

    fixed -y to apt

--- post_install/lib_python.sh
+++ post_install/lib_python.sh
@@ -6,16 +6,16 @@ yes | pip install pyinstaller
 yes | pip install PyCrypto 
 yes | pip install virtualenv 
 #lxml dependencies
-sudo apt-get install libxml2-dev libxslt-dev python-dev
+sudo apt-get install libxml2-dev libxslt-dev python-dev -y
 yes | pip install lxml
 yes | pip install beautifulsoup4
 yes | pip install networkx
 yes | pip install requests
 #yes | pip install numpy
 #matplotlib dependencies
-sudo apt-get build-dep python-matplotlib
+sudo apt-get build-dep python-matplotlib -y
 #yes | pip install matplotlib
 yes | pip install seaborn
 #pandas install is long and need sudo
 #yes | pip install pandas
-sudo apt-get install python-pandas
+sudo apt-get install python-pandas -y
