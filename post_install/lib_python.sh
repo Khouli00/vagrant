b49e32a47c20600780ee432fe5144aa699c7a23f
Khouli00 <supermoik@gmail.Com>
2016-01-19 13:23:03 +0100

    Update lib_python.sh
    apt instead of pip, remove some pip (comment)

--- post_install/lib_python.sh
+++ post_install/lib_python.sh
@@ -1,17 +1,21 @@
 ﻿
-#bs4 dep
+#bs4 dep and lxml
 apt-get install libxml2-dev libxslt1-dev python-dev -y
-
 yes | pip install pytest 
 yes | pip install pyinstaller 
 yes | pip install PyCrypto 
 yes | pip install virtualenv 
+#lxml dependencies
+sudo apt-get install libxml2-dev libxslt-dev python-dev
 yes | pip install lxml
 yes | pip install beautifulsoup4
 yes | pip install networkx
 yes | pip install requests
-yes | pip install numpy
-yes | pip install matplotlib
+#yes | pip install numpy
+#matplotlib dependencies
+sudo apt-get build-dep python-matplotlib
+#yes | pip install matplotlib
 yes | pip install seaborn
 #pandas install is long and need sudo
-yes | pip install pandas
+#yes | pip install pandas
+sudo apt-get install python-pandas
