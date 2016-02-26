9847d2dd1ed4cf485df972fd5c4285a762bcb47c
Khouli00 <supermoik@gmail.Com>
2016-02-26 07:09:13 +0100

    Update LanguageTools.sh

--- post_install/LanguageTools.sh
+++ post_install/LanguageTools.sh
@@ -7,10 +7,11 @@ cible="/home/vagrant/.vim/bundle/vim-grammarous/misc"
 sudo apt-get install default-jre
 
 #Download last version
-
+#FIXME : check last version available https://languagetool.org/download/LanguageTool-*
 mkdir $cible
 cd $cible
-wget https://www.languagetool.org/download/LanguageTool-3.0.zip
+wget https://languagetool.org/download/LanguageTool-3.2.zip
+
 unzip *.zip
 rm -f *.zip
 
