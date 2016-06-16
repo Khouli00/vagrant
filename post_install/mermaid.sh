b5562c3d0ee4b9e0a01eccc97e60a04dd5183b45
Khouli00 <supermoik@gmail.Com>
2016-06-16 21:52:14 +0200

    Update mermaid.sh

--- post_install/mermaid.sh
+++ post_install/mermaid.sh
@@ -1,4 +1,8 @@
-apt-get install phantomjs -y
+#install phantomjs 2.1
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xjvf phantomjs-2.1.1-linux-x86_64.tar.bz2
+mv phantomjs-2.1.1-linux-x86_64 /usr/local/share/
+ln -sf /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin
 apt-get install npm -y
 npm install -g mermaid
 #fix mermaid look for node
