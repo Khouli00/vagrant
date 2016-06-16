c1e0622e13b208279cb1c62c69a52adbe00cbd25
Khouli00 <supermoik@gmail.Com>
2016-06-16 21:37:00 +0200

    Update mermaid.sh

--- post_install/mermaid.sh
+++ post_install/mermaid.sh
@@ -1,2 +1,5 @@
-apt-get -y install phantomjs
-apt-get -y install npm
+apt-get install phantomjs -y
+apt-get install npm -y
+npm install -g mermaid
+#fix mermaid look for node
+ln -s /usr/bin/nodejs /usr/bin/node
