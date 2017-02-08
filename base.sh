2435cd9026dab00fef36d97e46376142db8106f0
Khouli00 <supermoik@gmail.Com>
2017-02-08 13:08:15 +0100

    Create base.sh

---
+++ base.sh
@@ -0,0 +1,12 @@
+apt-get update
+apt-get install build-essential -y
+apt-get install git -y
+apt-get install vim-gtk -y
+apt-get install dos2unix -y
+apt-get install unzip -y
+apt-get install exuberant-ctags -y
+apt-get install python-dev -y
+apt-get install python3-dev -y
+apt-get install python-logilab-common -y
+apt-get install zlib1g-dev -y
+apt-get install build-essential cmake -y
