757c267b24e2484f73c82140fddc68097fb05169
Khouli00 <supermoik@gmail.Com>
2017-02-11 12:52:25 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -1,12 +1,15 @@
 apt-get update
+#dev lib
 apt-get install build-essential -y
+apt-get install cmake -y
+apt-get install python-dev python3-dev
+apt-get install python-logilab-common -y
+apt-get install zlib1g-dev -y
+#utils
 apt-get install git -y
 apt-get install vim-gtk -y
 apt-get install dos2unix -y
 apt-get install unzip -y
 apt-get install exuberant-ctags -y
-apt-get install python-dev -y
-apt-get install python3-dev -y
-apt-get install python-logilab-common -y
-apt-get install zlib1g-dev -y
-apt-get install build-essential cmake -y
+
+
