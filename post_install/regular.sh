111e19092b4a767c4df7e15489bf19bcb56febd0
Khouli00 <supermoik@gmail.Com>
2017-02-11 09:01:17 +0100

    Update regular.sh
    clean up & YCM dep

--- post_install/regular.sh
+++ post_install/regular.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #time zone
-#interactive = sudo dpkg-reconfigure tzdata
+interactive = sudo dpkg-reconfigure tzdata
 timedatectl set-timezone Europe/Paris
 
 #nice color
-git clone https://github.com/Khouli00/color-theme-molokai ~
+#git clone https://github.com/Khouli00/color-theme-molokai ~
 
 #nice folder struct
 apt-get install tree
@@ -14,15 +14,15 @@ git config --global user.name "Khouli"
 git config --global user.email Khouli@VMdefault.com
 
 
-#git remote add VM https://github.com/Khouli00/vagrant.git
-#git clone https://github.com/Khouli00/nginx.git /etc/nginx
-
+#vim
 git clone https://github.com/Khouli00/vim ~/.vim	
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c VundleInstall -c quitall	
+#youcompleteme
+#note: should be done AFTER anaconda install
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
 
-#add asciidoc ctags support
-#https://github.com/majutsushi/tagbar/wiki#asciidoc
 cp .ctags ~/
 
 
