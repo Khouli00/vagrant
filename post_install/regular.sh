15c5ef1b971105feb145b86c397b489707d16e2d
Khouli00 <supermoik@gmail.Com>
2017-03-18 22:18:38 +0100

    remove autocompleteme

--- post_install/regular.sh
+++ post_install/regular.sh
@@ -18,9 +18,5 @@ git config --global user.email Khouli@VMdefault.com
 git clone https://github.com/Khouli00/vim ~/.vim	
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c VundleInstall -c quitall	
-#youcompleteme
-#https://github.com/Valloric/YouCompleteMe#ubuntu-linux-x64
-#note: should be done AFTER anaconda install
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
+
 
