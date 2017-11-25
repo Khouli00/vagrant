37a43bf5d3a426a46df3e192698d50a2868c88af
Khouli00 <supermoik@gmail.Com>
2017-11-25 18:58:54 +0100

    Update YouCompleteMe.sh

--- post_install/YouCompleteMe.sh
+++ post_install/YouCompleteMe.sh
@@ -1,6 +1,4 @@
 sudo apt-get install build-essential cmake -y
 sudo apt-get install python-dev -y
 cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-cd ~/.vim/bundle/YouCompleteMe
 ./install.py
